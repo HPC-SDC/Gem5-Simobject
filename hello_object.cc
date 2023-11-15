a8d84e4db526828cba2bcdd6917d718c0df2c877
Owen-Kinggg <150303558+Owen-Kinggg@users.noreply.github.com>
2023-11-15 21:46:44 +0800

    Add files via upload

---
+++ hello_object.cc
@@ -0,0 +1,14 @@
+#include "learning_gem5/hello_object.hh"
+
+#include <iostream>
+
+namespace gem5
+{
+
+    HelloObject::HelloObject(const HelloObjectParams& params) :
+        SimObject(params)
+    {
+        std::cout << "Hello World! From a SimObject!" << std::endl;
+    }
+
+} // namespace gem5
