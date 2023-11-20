2c5174cfd121e62e76e81916296450cb3e33935b
Owen-Kinggg <150303558+Owen-Kinggg@users.noreply.github.com>
2023-11-20 12:00:55 +0800

    Update hello_object.cc

--- hello_object.cc
+++ hello_object.cc
@@ -1,14 +1,55 @@
 #include "learning_gem5/hello_object.hh"
+#include "base/trace.hh"
+#include "debug/Hello.hh"
 
 #include <iostream>
 
 namespace gem5
 {
-
-    HelloObject::HelloObject(const HelloObjectParams& params) :
-        SimObject(params)
+    /*
+    MyObject::MyObject(const MyObjectParams& params) :
+        SimObject(params), 
+        //event(*this),
+        event([this] {processEvent(); }, name()),
+        myName(params.name),
+        latency(params.time_to_wait),
+        timesLeft(params.number_of_fires)
     {
-        std::cout << "Hello World! From a SimObject!" << std::endl;
+        DPRINTF(Hello, "Created the hello object\n");
+        //std::cout << "Hello World! From a SimObject!" << std::endl;
+    }
+    */
+
+    MyObject::MyObject(MyObjectParams& params) :
+        SimObject(params),
+        event(*this),
+        goodbye(params.goodbye_object),
+        myName(params.name),
+        latency(params.time_to_wait),
+        timesLeft(params.number_of_fires)
+    {
+        DPRINTF(Hello, "Created the hello object with the name %s\n", myName);
+        panic_if(!goodbye, "HelloObject must have a non-null GoodbyeObject");
+    }
+
+
+    void MyObject::processEvent()
+    {
+        timesLeft--;
+        DPRINTF(Hello, "Hello world! Processing the event! %d left\n", timesLeft);
+
+        if (timesLeft <= 0) {
+            DPRINTF(Hello, "Done firing!\n");
+            goodbye->sayGoodbye(myName);
+        }
+        else {
+            schedule(event, curTick() + latency);
+        }
+    }
+
+    void MyObject::startup()
+    {
+        schedule(event, latency);
     }
 
 } // namespace gem5
