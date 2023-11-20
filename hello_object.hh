473961e7fc13dc67f99a30b23582cb8c3ddf49bd
Owen-Kinggg <150303558+Owen-Kinggg@users.noreply.github.com>
2023-11-20 12:01:18 +0800

    Update hello_object.hh

--- hello_object.hh
+++ hello_object.hh
@@ -1,18 +1,64 @@
 #ifndef __LEARNING_GEM5_HELLO_OBJECT_HH__
 #define __LEARNING_GEM5_HELLO_OBJECT_HH__
 
-#include "params/HelloObject.hh"
+#include "params/MyObject.hh"
 #include "sim/sim_object.hh"
+#include "learning_gem5/goodbye_object.hh"
 
+/*
 namespace gem5
 {
 
-    class HelloObject : public SimObject
+    class MyObject : public SimObject
     {
+
+    private:
+        void processEvent();
+
+        EventFunctionWrapper event;
+
+        const std::string myName;
+
+        Tick latency;
+
+        int timesLeft;
+
+
     public:
-        HelloObject(const HelloObjectParams& p);
+        MyObject(const MyObjectParams& p);
+
+        void startup() override;
+
     };
 
 } // namespace gem5
+*/
+
+class MyObject : public SimObject
+{
+private:
+    void processEvent();
+
+    EventWrapper event;
+
+    /// Pointer to the corresponding GoodbyeObject. Set via Python
+    GoodbyeObject* goodbye;
+
+    /// The name of this object in the Python config file
+    const std::string myName;
+
+    /// Latency between calling the event (in ticks)
+    const Tick latency;
+
+    /// Number of times left to fire the event before goodbye
+    int timesLeft;
+
+public:
+    MyObject(const MyObjectParams& p);
+
+    void startup() override;
+};
+
+
 
 #endif // __LEARNING_GEM5_HELLO_OBJECT_HH__
